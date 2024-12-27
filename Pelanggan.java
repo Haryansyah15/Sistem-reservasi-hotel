2694d09771ab4b2badd8a8745e2301d1eeb4bca1
Haryansyah15 <165676634+Haryansyah15@users.noreply.github.com>
2024-12-27 09:10:44 +0700

    Pelanggan.java

--- Pelanggan.java
+++ Pelanggan.java
@@ -1,8 +1,35 @@
-class Pelanggan {
-    String nama;
+class Pelanggan extends Manusia{
     String alamat;
     String email;
-    public Pelanggan(String nama, String alamat, String email) {
-        this.nama = nama; this.alamat = alamat; this.email = email;
+
+    public Pelanggan(String nik, String nama, String no_hp, String alamat, String email) {
+        super(nik, nama, no_hp);
+        this.alamat = alamat;
+        this.email = email;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void tampilkanInfo() {
+        System.out.println("NIK: " + getNik());
+        System.out.println("Nama: " + getNama());
+        System.out.println("No HP: " + getNoHp());
+        System.out.println("Alamat: " + getAlamat());
+        System.out.println("Email: " + getEmail());
     }
 }
