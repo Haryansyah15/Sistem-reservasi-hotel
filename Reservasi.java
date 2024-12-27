4b44027a65f0a5c06ad0a96337c8978d5e701cd1
Haryansyah15 <165676634+Haryansyah15@users.noreply.github.com>
2024-12-27 09:13:10 +0700

    Reservasi.java

--- Reservasi.java
+++ Reservasi.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+
 class Reservasi {
     Pelanggan pelanggan;
     Kamar kamar;
@@ -6,18 +7,34 @@ class Reservasi {
     LocalDate tanggalCheckout;
     double totalBiaya;
     String statusReservasi;
+
     public Reservasi(Pelanggan pelanggan, Kamar kamar, LocalDate tanggalCheckin, LocalDate tanggalCheckout) {
         this.pelanggan = pelanggan;
         this.kamar = kamar;
         this.tanggalCheckin = tanggalCheckin;
         this.tanggalCheckout = tanggalCheckout;
-        this.statusReservasi = "Dipesan"; hitungBiaya();
+        this.statusReservasi = "Dipesan";
+        hitungBiaya();
     }
+
     public void hitungBiaya() {
         long lamaMenginap = tanggalCheckout.toEpochDay() - tanggalCheckin.toEpochDay();
         totalBiaya = lamaMenginap * kamar.hargaPerMalam;
     }
+
     public boolean validasiReservasi() {
         return !tanggalCheckin.isAfter(tanggalCheckout);
+    }
+
+    // Method to get reservation details as a string
+    public String getDetails() {
+        return "Nama: " + pelanggan.getNama() + "\n" +
+                "Alamat: " + pelanggan.alamat + "\n" +
+                "Email: " + pelanggan.email + "\n" +
+                "Kamar: " + kamar.tipeKamar + "\n" +
+                "Tanggal Check-in: " + tanggalCheckin + "\n" +
+                "Tanggal Check-out: " + tanggalCheckout + "\n" +
+                "Total Biaya: " + totalBiaya + "\n" +
+                "Status: " + statusReservasi + "\n";
     }
 }
