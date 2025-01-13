baf1eaf076bcf4e3e5bfee82b781c77e4ac951b3
Irfan Habibi <168463120+BeeBeeCuy@users.noreply.github.com>
2025-01-13 13:10:28 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,10 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ReservasiHotel reservasiHotel = new ReservasiHotel();
 
-        // Add some rooms to the hotel for testing
-        reservasiHotel.tambahKamar(new Kamar(101, "Deluxe", 200000));
-        reservasiHotel.tambahKamar(new Kamar(102, "Suite", 350000));
-        reservasiHotel.tambahKamar(new Kamar(103, "Standart", 500000));
-        reservasiHotel.tambahKamar(new Kamar(104, "Family Room", 850000));
-
-        // Create the GUI
-        new ReservasiGUI(reservasiHotel) {
-            @Override
-            public void buatReservasi() {
-
-            }
-
-            @Override
-            public void batalkanReservasi(String nama) {
-
-            }
-        };
+        SwingUtilities.invokeLater(() -> new ReservasiGUI());
     }
 }
